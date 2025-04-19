c47115e4aa056d791fd515a7c6246239a209e162
iadev2005 <idaliendres.23@est.ucab.edu.ve>
2025-04-19 00:19:37 -0400

    Terminado el curso introductorio de React

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,27 +1,145 @@
 import './App.css'
-
+import { TwitterFollowCard } from './TwitterFollowCard'
+import { useState } from 'react'
 // Export es para exportar el componente
 
 // Hay dos maneras de estilizar en React, la primera es con estilos en línea, pero lo tendríamos que hacer como si fueran objetos, y la segunda es con un archivo css
 
 // Para evitar conflictos con los selectores usaremos los className
 
+// El <> </> es equivalente a usar el React.Fragment
+
+// export function App() {
+//     return (
+//         <>
+//             <TwitterFollowCard name="Miguel Ángel Durán" userName="midudev" isFollowing="Siguiendo"/>
+//             <TwitterFollowCard name="Lionel Andrés Messi" userName="messi" isFollowing="Siguiendo"/>
+//             <TwitterFollowCard name="El Rubius" userName="rubius" isFollowing="Siguiendo"/>
+//         </>
+
+//     )
+// }
+
+// Como es un booleano basta con pasar isFollowing (true) es suficiente, no existe la forma negada de manera directa, la otra opción es isFollowing={true}
+
+// El formatUserName es para darnos cuenta que también podemos enviar funciones como parámetros (callbacks), en este caso, para modificar una parte de la componente según el contexto. Acá le pasamos una función que ejecutará en TwitterFollowCard.jsx
+
+// React renderiza elementos
+// Componente: Factoría de los elementos
+// Elemento: Producto final de un componente
+
+// export function App() {
+//     const formatUserName = (userName) => `@${userName}`
+//     return (
+//         <section className='App'>
+//             <TwitterFollowCard formatUserName={formatUserName} name={"Miguel Ángel Durán"} userName={"midudev"} isFollowing />
+//             <TwitterFollowCard formatUserName={formatUserName} name={"Lionel Andrés Messi"} userName={"messi"} isFollowing />
+//             <TwitterFollowCard formatUserName={formatUserName} name={"El Rubius"} userName={"rubius"} isFollowing={false} />
+//         </section>
+//     )
+// }
+
+// Supongamos que queremos pasar el nombre envuelto
+// export function App () {
+//     // Otra manera de pasar la prop, es como un objeto, esto no es recomendable, a menos, que sean muchas prop
+//     const messi = {isFollowing: true, userName: "Messi"}
+//     return (
+//         <section className='App'>
+//             <TwitterFollowCard isFollowing userName={"midudev"}>
+//                 Miguel Ángel Durán
+//             </TwitterFollowCard>
+//             <TwitterFollowCard {... messi}>
+//                 Lionel Andrés Messi
+//             </TwitterFollowCard>
+//             <TwitterFollowCard isFollowing={false}>
+//                 Unknown
+//             </TwitterFollowCard>
+//         </section>
+//     )
+// }
+
+// Ahora en vez de que isFollowing sea un prop, haremos que sea un estado
+// export function App () {
+//     // Otra manera de pasar la prop, es como un objeto, esto no es recomendable, a menos, que sean muchas prop. Para eso importaremos una utilidad llamada useState, eso lo haremos en TwitterFollowCard.jsx
+
+//     // Otra cosa interesante, es que como ya vimos en TwitterFollowCard.jsx, cuando hacemos un cambio de estado, este solo afecta a la componente asociada, pero, si hicieramos eso aquí, donde estamos almacenando a todas las componentes, ese cambio afecta a todos las componentes, ejemplo:
+
+//     // Cambio de estado:
+//     const [name, setName] = useState('midudev')
+
+//     console.log('Render with name: ', name)
+
+//     // La función flecha se puede hacer tanto afuera como dentro del return
+
+//     // Ojo, renderizar desde acá no quiere decir que estamos sobre-escribiendo la app, simplemente renderizamos los nuevos elementos, React entiende que si no se hicieron cambios no hace falta modificar esa parte del DOOM
+
+//     // Cuando usamos una prop para inicializar un estado usamos initial como prefijo
+
+//     return (
+//         <section className='App'>
+//             <TwitterFollowCard userName={name}>
+//                 Miguel Ángel Durán
+//             </TwitterFollowCard>
+//             <TwitterFollowCard userName={"messi"} initialIsFollowing= {true}>
+//                 Lionel Andrés Messi
+//             </TwitterFollowCard>
+//             <TwitterFollowCard>
+//                 Unknown
+//             </TwitterFollowCard>
+//             <button onClick={() => setName('messi')}>
+//                 Cambio de Nombre
+//             </button>
+//         </section>
+//     )
+// }
+
+// Un concepto fundamental es entender que una prop solo se inicializa una vez, es decir, si le vamos a hacer cambios, debe ser desde el componente hijo en este caso TwitterFollowCard.jsx, esto con la función setIsFollowing, hacerlo desde App.jsx, no funcionará ya que solo lo inicializará la primera vez y después no tendrá control sobre lo que pasa en el hijo
+
+// Acá dejaré el código limpio
+
+// Un caso real suele darse cuando hay un array de elementos, en estos casos se trabaja así
+
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán',
+        isFollowing: true
+    },
+    {
+        userName: 'pheralb',
+        name: 'Pablo H.',
+        isFollowing: false
+    },
+    {
+        userName: 'PacoHdezs',
+        name: 'Paco Hdez',
+        isFollowing: true
+    },
+    {
+        userName: 'TMChein',
+        name: 'Tomas',
+        isFollowing: false
+    }
+]
+
+// Para usar este array lo podemos mapear con JS (se esta retornando de manera directa)
+
+// Cuando se mapea se puede hacer uso de un index, pero esto no se suele recomendar en estos casos ya que existe el peligro que se repita. Lo mejor es usar como key algo que sepamos que es único de ese elemento. Usar un math random también es mala práctica, si usamos índices lo debemos declarar en el campo del objeto. De lejos lo más profesional sería una id de una BD
 export function App() {
     return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img className='tw-followCard-avatar' alt="El avatar de midudev" src="https://unavatar.io/midudev"/>
-                <div className='tw-followCard-info'>
-                    <strong>Miguel Ángel Durán</strong>
-                    <span className='tw-followCard-infoUserName'>@midudev</span>
-                </div>
-            </header>
-
-            <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <section className='App'>
+            {
+                users.map(({ userName, name, isFollowing }) => (
+                    <TwitterFollowCard
+                    // Es obligatorio usar el key (identificador único) para que react diferencie los elementos del mapeo, esto solo porque estamos mapeando
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
+                ))
+            }
+        </section>
     )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,17 +1,119 @@
-export function TwitterFollowCard() {
+import { useState } from "react"
+
+// Importación para el manejo de estados (hook)
+
+// Los hooks es para ejecutar código arbitrario cuando hay cierto evento con nuestras componentes
+
+// Los props son los parámetros modificables del componente
+
+// isFollowing es un valor booleano, y si no se pasa queda como undefined
+
+// Si no pasamos el @ por acá lo podemos poner en App.jsx, o, que queremos poner un formato según el contexto
+// export function TwitterFollowCard({ formatUserName, name, userName ,isFollowing}) {
+//     const imageSrc = `https://unavatar.io/${userName}`
+//     // Nunca modificar una prop, mejor usar una constante como lo hicimos con imageSrc
+//     // name = `soy$(name)`
+//     // Supongamos que nos pasaron un arreglo de número (numbers), y que al recibirlo le hagamos un push para agregar otro número, esto serí un grave error, ya que modificamos las props, las props deben ser inmutables
+//     return (
+//         <article className='tw-followCard'>
+//             <header className='tw-followCard-header'>
+//                 <img className='tw-followCard-avatar' alt="El avatar de midudev" src={imageSrc} />
+//                 <div className='tw-followCard-info'>
+//                     <strong>{name}</strong>
+//                     <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
+//                 </div>
+//             </header>
+
+//             <aside>
+//                 <button className='tw-followCard-button'>
+//                     Seguir
+//                 </button>
+//             </aside>
+//         </article>
+//     )
+// }
+
+// Con children recuperamos el texto envuelto hijo de la etiqueta twitter, aunque claro, esto en este caso, podríamos extraer cualquier cosa de children
+
+// Se puede poner el valor unknown en caso de que no se reciba un userName
+// export function TwitterFollowCard({children, userName = 'unknown', initialIsFollowing}) {
+//     // Creamos nuestro estado, por defecto no se sigue a una persona, así que será false
+//     // Usaremos la desempaquetación de JS para ahorrar líneas de código
+//     const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+//     // Esto nos devuelve un array, en la primera posición tenemos el valor del estado (estado)
+//     // const isFollowing = state[0]
+//     // En la segunda posición tenemos una función que nos permitirá actualizar el estado (interruptor)
+//     // const setIsFollowing = state[1]
+
+//     // Acá detectamos los cambios del render
+//     console.log('[TwitterFollowCard] render with userName: ', userName)
+
+//     const imageSrc = `https://unavatar.io/${userName}`
+//     // Nunca modificar una prop, mejor usar una constante como lo hicimos con imageSrc
+//     // name = `soy$(name)`
+//     // Supongamos que nos pasaron un arreglo de número (numbers), y que al recibirlo le hagamos un push para agregar otro número, esto serí un grave error, ya que modificamos las props, las props deben ser inmutables
+//     const text = isFollowing ? 'Siguiendo' : 'Seguir'
+//     const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+
+//     // Ahora para cambiar el estado lo que esperamos es un click, pero en vez de usar el clásico addEventListener de JS, podemos usar directamente onClick en el elemento button, ya que React es declarativo
+//     const handleClick = () => {
+//         setIsFollowing(!isFollowing) //Invertimos el valor del estado
+//     }
+//     return (
+//         <article className='tw-followCard'>
+//             <header className='tw-followCard-header'>
+//                 <img className='tw-followCard-avatar' alt="El avatar de midudev" src={imageSrc} />
+//                 <div className='tw-followCard-info'>
+//                     <strong>{children}</strong>
+//                     <span className='tw-followCard-infoUserName'>{`@${userName}`}</span>
+//                 </div>
+//             </header>
+
+//             <aside>
+//                 <button className={buttonClassName} onClick={handleClick}>
+//                     {text}
+//                 </button>
+//             </aside>
+//         </article>
+//     )
+// }
+
+// Otro concepto importante son las prop children, por ejemplo en este momento "seguir" es hijo de button, y button a su vez es hijo de aside.
+
+// Bueno ahora dejaré el código lo más limpio posible
+
+export function TwitterFollowCard({ children, userName, initialIsFollowing }) {
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+
+    console.log('[TwitterFollowCard] render with userName: ', userName)
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing
+        ? 'tw-followCard-button is-following'
+        : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
-                <img className='tw-followCard-avatar' alt="El avatar de midudev" src="https://unavatar.io/midudev" />
+                <img
+                    className='tw-followCard-avatar'
+                    alt='El avatar de midudev'
+                    src={`https://unavatar.io/${userName}`}
+                />
                 <div className='tw-followCard-info'>
-                    <strong>Miguel Ángel Durán</strong>
-                    <span className='tw-followCard-infoUserName'>@midudev</span>
+                    <strong>{children}</strong>
+                    <span className='tw-followCard-infoUserName'>@{userName}</span>
                 </div>
             </header>
 
             <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className='tw-followCard-text'>{text}</span>
+                    <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
                 </button>
             </aside>
         </article>
